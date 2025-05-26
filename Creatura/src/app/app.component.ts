69d48fc38f96e5ff24cd9963965f95a3f5184a38
Tabare8031 <facundovuerra@gmail.com>
2025-05-26 20:34:37 -0300

    La porquería esta (no está terminada)
    Los colores son temporales

--- Creatura/src/app/app.component.ts
+++ Creatura/src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { BarraBusquedaComponent } from "./components/barra-busqueda/barra-busqueda.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, BarraBusquedaComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
---
+++ Creatura/src/app/components/barra-busqueda/barra-busqueda.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-barra-busqueda',
+  imports: [],
+  templateUrl: './barra-busqueda.component.html',
+  styleUrl: './barra-busqueda.component.scss'
+})
+export class BarraBusquedaComponent {
+
+}
