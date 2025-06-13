1ee9d0daf5f55e2efb6e46deb49b6136b12e9057
Tabare8031 <facundovuerra@gmail.com>
2025-06-13 20:58:06 -0300

    Kakashi...
    el ninja que copia

--- Creatura/src/app/components/inicio/inicio.component.ts
+++ Creatura/src/app/components/inicio/inicio.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ConeccionService } from '../../serviceses/coneccion.service';
 
 @Component({
   selector: 'app-inicio',
@@ -6,6 +7,28 @@ import { Component } from '@angular/core';
   templateUrl: './inicio.component.html',
   styleUrl: './inicio.component.scss'
 })
-export class InicioComponent {
+export class InicioComponent implements OnInit{
+  creaturas: any[] = [];
+  constructor(private connector: ConeccionService) {}
+
+  onImgError(event: Event) {
+    const element = event.target as HTMLImageElement;
+    element.src = 'defoult.png'; // Ruta de imagen por defecto
+  }
+
+  ngOnInit(): void {
+    this.connector.listadoCreaturaConTipos().subscribe((res) => {
+      this.randomizador(res);
+      this.creaturas = res.slice(0, 20); //limitador
+      console.log(this.creaturas);
+    });
+  }
+
+  private randomizador(array: any[]): void {
+    for (let i = array.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1)); 
+      [array[i], array[j]] = [array[j], array[i]]; 
+    }
+  }
 
 }
